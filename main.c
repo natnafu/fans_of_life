3e5e5a2b3ddd63b27e11b159f3ca14c16b34d69e
Nat Atnafu <natnafu@gmail.com>
2024-07-19 17:13:35 -0400

    Unified master/cell code into one that reads DIP switches
    Single binary for all boards. If the ISM switch is set, the board will
    assume the role of the master. Otherwise it will be a cell and get its
    address based on the 3 address pins on the switch.

--- main.c
+++ main.c
@@ -1,14 +1,9 @@
 /* ========================================
- *
- * Copyright NAFU STUDIOS, 2018
- * All Rights Reserved
- * UNPUBLISHED, LICENSED SOFTWARE.
- *
- * CONFIDENTIAL AND PROPRIETARY INFORMATION
- * WHICH IS THE PROPERTY OF NAFU STUDIOS.
- *
+ * Fans of Life, 2024
+ * Nafu Studios
+ * Nat Atnafu
  * ========================================
-*/
+ */
 #include <stdlib.h>
 
 #include "conway.h"
@@ -19,212 +14,216 @@
 #include "rs485.h"
 #include "stopwatch.h"
 
-#define IS_SLAVE
-//#define IS_MASTER
-
-// Config error checking
-#if (defined IS_SLAVE && defined IS_MASTER)
-#error  Can not define both master and slave   
-#elif (defined IS_MASTER && UART_RX_HW_ADDRESS1 != 8)
-#error  incorrect master UART hardware address
-#endif
+// time to wait for human input to finish
+#define CHANGE_TIMER_MS 6000
 
 // Cell configuration bits
-#define CONFIG_PULSE_TIME   (1 << 0)    // enables/disables pulsing all fans on before a read operation
-#define CONFIG_PWM_FANS     (1 << 1)    // enables/disables PWMing fans that are on
+#define CONFIG_PULSE_TIME (1 << 0) // enables/disables pulsing all fans on before a read operation
+#define CONFIG_PWM_FANS   (1 << 1) // enables/disables PWMing fans that are on
 
-#define CONFIG_DEFAULT      0           // default cell config, no pulsing or pwm
+#define CONFIG_DEFAULT 0 // default cell config, no pulsing or pwm
 
 int main(void) {
 
-    CyGlobalIntEnable;          // Enable global interrupts.
-    Timer_Start();              // Used for stopwatch timers
+  CyGlobalIntEnable; // Enable global interrupts.
+  Timer_Start(); // Used for stopwatch timers
 
-    // Used for RS485 comms
-    UART_Start();
-    // Get hardware address
-    uint8_t uart_address = 0;
+  char buf[1];
+
+  // Used for debug messages
+  UART_DEBUG_Start();
+  UART_DEBUG_PutString("\nprogram start\n");
+
+  uint8_t is_master = Pin_ISM_Read();
+
+  // Get hardware address
+  uint8_t uart_address = 0;
+  if (is_master) {
+    uart_address = 8;
+    UART_DEBUG_PutString("\nis_master");
+  } else {
     if (Pin_A0_Read()) uart_address |= 0b00000001;
     if (Pin_A1_Read()) uart_address |= 0b00000010;
     if (Pin_A2_Read()) uart_address |= 0b00000100;
-    UART_SetRxAddress1(uart_address);
-    UART_SetRxAddress2(uart_address);
-    UART_ClearRxBuffer();
+    UART_DEBUG_PutString("\nis_cell ");
+    itoa(uart_address,buf,10);
+    UART_DEBUG_PutChar(buf[0]);
+  }
 
-    // Used for debug messages
-    UART_DEBUG_Start();
-    UART_DEBUG_PutString("\nstart\n");
+  // Used for RS485 comms
+  UART_Start();
+  UART_SetRxAddress1(uart_address);
+  UART_SetRxAddress2(uart_address);
+  UART_ClearRxBuffer();
 
-#if (defined IS_SLAVE)
-    PWM_FAN_SPEED_Start();
-    PWM_FAN_SPEED_WriteCompare(90);
-    
-    uint8_t rx_buff_size = 0;   // Stores UART RX buffer size
-    uint32_t ctrl_state = 0;    // commanded state from master
-    uint32_t old_state  = 0;    // previous state
-    uint32_t curr_state = 0;    // current state
+  // used by master
+  uint32_t timer_change; // counts the time since human input
+  // used by cell
+  uint8_t rx_buff_size = 0; // Stores UART RX buffer size
+  uint32_t ctrl_state = 0; // commanded state from master
+  uint32_t old_state = 0; // previous state
+  uint32_t curr_state = 0; // current state
+  uint32_t config = CONFIG_DEFAULT;
+  uint32_t cell_pulse_time_ms = 0;
+  uint32_t cell_pwm_time_ms = 0;
+  uint32_t timer_comm = 0; // RS485 communication timer
+  uint32_t validation[FANS_PER_CELL] = {0}; // holds the validation times for each fan
 
-    uint32_t config = CONFIG_DEFAULT;
-    uint32_t cell_pulse_time_ms = 0;
-    uint32_t cell_pwm_time_ms = 0;
-
-    uint32_t timer_comm = 0;    // RS485 communication timer 
-
-    uint32_t validation[FANS_PER_CELL] = {0};    // holds the validation times for each fan
-
-    gpiox_init();   // Init GPIO expander ICs
-    curr_state = fan_set_state(0, TOUT_FAN_SET);     // Init fan states to 0
-#elif (defined IS_MASTER)
+  if (is_master) {
     CyDelay(1000);
     master_write_all(0);
-    
-#define CHANGE_TIMER_MS     6000
-    uint32_t timer_change = stopwatch_start();   // counts the time since human input
-    //CyDelay(6000);
-#endif
+    timer_change = stopwatch_start();
+  } else {
+    PWM_FAN_SPEED_Start();
+    PWM_FAN_SPEED_WriteCompare(90);
+    gpiox_init(); // Init GPIO expander ICs
+    curr_state = fan_set_state(0, TOUT_FAN_SET); // Init fan states to 0
+  }
 
-    for(;;)
-    {
-#ifdef IS_SLAVE
-        // Handle pulsing. Turns all fans on for cell_pulse_time_ms and then restores state.
-        // This is attempting to quiet the whining sound of the fans when off and trying to spin.
-        // Depending on length, may create an audible clicking sound or have no effect.
-        // NOTE: this doesn't really work :(
-        if (cell_pulse_time_ms) {
-            fan_set_state(UINT32_MAX, 0);
-            CyDelay(cell_pulse_time_ms);
-            fan_set_state(curr_state, 0);
-        }
+  for (;;)
+  {
+    if (is_master) {
+      // Play Conway's Game of Life
 
-        // Handle PWMing. If enabled, turn on fan for cell_pwm_time_ms and then turn off.
-        // This lowers the fan speed for less force on fingers. Trade off is a small audible clicking sound.
-        if (cell_pwm_time_ms) {
-            fan_set_state(curr_state, 0);
-            CyDelay(cell_pwm_time_ms);
-            fan_set_state(0,0);
-        }
+      // Save last state
+      memcpy(conway_last_frame, conway_curr_frame, sizeof(conway_curr_frame));
 
-        // Handle updating fan state
-        old_state = curr_state;         // Save state
-        curr_state = fan_get_state();   // Get new state (200ms blocking time)
+      // Read grid
+      master_read_grid(conway_curr_frame);
 
-        // Handle validation
-        for (uint32_t i = 0; i < FANS_PER_CELL; i++) {
-            if (validation[i]) {
-                if (curr_state & (1 << i)) {
-                    // fan still spinning, check if validation expired
-                    if (stopwatch_elapsed_ms(validation[i]) >= TOUT_FAN_SET) {
-                        // validation expired, reset
-                        validation[i] = 0;
-                    } else {
-                        // still validating, don't recognize spindown as human input
-                        curr_state &= ~(1 << i);
-                    }
-                } else {
-                    // fan not spinning, reset validation
-                    validation[i] = 0;
-                }
-            }
-        }
+      // Check if it's change
+      if (conway_has_changed() >= 2) {
+        timer_change = stopwatch_start();
+      }
 
-        // Handle human input
-        if (curr_state != old_state) {
-            fan_set_state(curr_state, 0);  // don't validate since human input has no spindown
-        }
-
-        // Check for commands
-        rx_buff_size = UART_GetRxBufferSize();
-        if (rx_buff_size == UART_RX_BUFFER_SIZE) {
-            UART_DEBUG_PutString("\data received\n");
-            uint8 rx_cmd = UART_ReadRxData();   // first byte is READ/WRITE
-            if (rx_cmd == UART_READ) {
-                rs485_tx(MASTER_ADDRESS, UART_READ, curr_state);    // send back current state
-            } else if (rx_cmd == UART_WRITE) {
-                // next 4 bytes are new state
-                ctrl_state  = ((uint32_t) UART_ReadRxData() << 24) |
-                              ((uint32_t) UART_ReadRxData() << 16) |
-                              ((uint32_t) UART_ReadRxData() <<  8) |
-                              ((uint32_t) UART_ReadRxData() <<  0);
-                rs485_tx(MASTER_ADDRESS, UART_WRITE, curr_state);   // send back confirmation that cmd was received
-                curr_state = fan_set_ctrl(curr_state, ctrl_state, validation);
-            } else if (rx_cmd == UART_CONFIG) {
-                uint8_t config_option =  UART_ReadRxData(); // get type of config
-                if (config_option == CONFIG_PULSE_TIME) {
-                    cell_pulse_time_ms = UART_ReadRxData();
-                } else if (config_option == CONFIG_PWM_FANS) {
-                    cell_pwm_time_ms = UART_ReadRxData();
-                }
-            }
-            // Clear RX buffer after processing data
-            UART_ClearRxBuffer();
-            timer_comm = 0;
-        } else if (rx_buff_size != 0) {
-            // If buffer isn't full or empty, clear the buffer if a timeout occurs
-            if (timer_comm == 0) {
-                // Set timer if it hasn't been started yet
-                timer_comm = stopwatch_start();
-            } else {
-                if (stopwatch_elapsed_ms(timer_comm) >= TOUT_RX_COMM) {
-                    // Comm timeout: reset timer and clear RX buffer
-                    timer_comm = 0;
-                    UART_ClearRxBuffer();    
-                }
-            }
-        }
-#endif // SLAVE
-
-#ifdef IS_MASTER 
-        // Play Conway's Game of Life
-
-        // Save last state
-        memcpy(conway_last_frame, conway_curr_frame, sizeof(conway_curr_frame));
-        // Read grid
-        master_read_grid(conway_curr_frame);
-        // Check if it's change        
-        if (conway_has_changed() >= 2) {
-            timer_change = stopwatch_start();    
-        }
-        // Update only if timer has expired
-        if (stopwatch_elapsed_ms(timer_change) >= CHANGE_TIMER_MS) {
-            conway_update_frame();
-            master_write_grid(conway_curr_frame);
-            timer_change = stopwatch_start();
-        }
-/*
-        // Tests turning all fans on and off
-        while(1) {
-            master_write_all(0);
-            CyDelay(5000);
-            master_write_all(UINT32_MAX);
-            CyDelay(5000);
-        }
-               
-        // Tests conway wiht no human input
-        memcpy(conway_curr_frame, conway_dead, sizeof(conway_curr_frame));
+      // Update only if timer has expired
+      if (stopwatch_elapsed_ms(timer_change) >= CHANGE_TIMER_MS) {
+        conway_update_frame();
         master_write_grid(conway_curr_frame);
-        CyDelay(6000);
-        while(1) {
-            conway_update_frame();
-            master_write_grid(conway_curr_frame);
-            CyDelay(6000);
-        }
+        timer_change = stopwatch_start();
+      }
+      /*
+              // Tests turning all fans on and off
+              while(1) {
+                  master_write_all(0);
+                  CyDelay(5000);
+                  master_write_all(UINT32_MAX);
+                  CyDelay(5000);
+              }
 
-        // Tests writing cells
-        uint32_t state_test;
-        for (uint8_t cell = 0; cell < NUM_CELLS; cell++) {
-        //for (uint8_t cell = 1; cell < 2; cell++) {
-            state_test = 0;
-            for (uint8_t fan = 0; fan < FANS_PER_CELL; fan++) {
-                state_test |= (1 << fan);
-                master_write_cell(cell, state_test);
-                CyDelay(500);   // min slave response time is ~200ms because of blocking fan_get_state()
-                // NOTE: this delay only works for setting. To be robust to all commands, this delay should be
-                // greater than or equal to the fan validation timeout. Possibly could have the slave respond when
-                // either the state has been set or its validation has timed out. All robust solutions result in
-                // the grid getting updated every ~5s (spindown time).
+              // Tests conway with no human input
+              memcpy(conway_curr_frame, conway_dead, sizeof(conway_curr_frame));
+              master_write_grid(conway_curr_frame);
+              CyDelay(6000);
+              while(1) {
+                  conway_update_frame();
+                  master_write_grid(conway_curr_frame);
+                  CyDelay(6000);
+              }
+
+              // Tests writing cells
+              uint32_t state_test;
+              for (uint8_t cell = 0; cell < NUM_CELLS; cell++) {
+              //for (uint8_t cell = 1; cell < 2; cell++) {
+                  state_test = 0;
+                  for (uint8_t fan = 0; fan < FANS_PER_CELL; fan++) {
+                      state_test |= (1 << fan);
+                      master_write_cell(cell, state_test);
+                      CyDelay(500);   // min slave response time is ~200ms because of blocking fan_get_state()
+                      // NOTE: this delay only works for setting. To be robust to all commands, this delay should be
+                      // greater than or equal to the fan validation timeout. Possibly could have the slave respond when
+                      // either the state has been set or its validation has timed out. All robust solutions result in
+                      // the grid getting updated every ~5s (spindown time).
+                  }
+              }
+      */
+
+    } else {
+      // Handle pulsing. Turns all fans on for cell_pulse_time_ms and then restores state.
+      // This is attempting to quiet the whining sound of the fans when off and trying to spin.
+      // Depending on length, may create an audible clicking sound or have no effect.
+      // NOTE: this doesn't really work :(
+      if (cell_pulse_time_ms) {
+        fan_set_state(UINT32_MAX, 0);
+        CyDelay(cell_pulse_time_ms);
+        fan_set_state(curr_state, 0);
+      }
+
+      // Handle PWMing. If enabled, turn on fan for cell_pwm_time_ms and then turn off.
+      // This lowers the fan speed for less force on fingers. Trade off is a small audible clicking sound.
+      if (cell_pwm_time_ms) {
+        fan_set_state(curr_state, 0);
+        CyDelay(cell_pwm_time_ms);
+        fan_set_state(0, 0);
+      }
+
+      // Handle updating fan state
+      old_state = curr_state; // Save state
+      curr_state = fan_get_state(); // Get new state (200ms blocking time)
+
+      // Handle validation
+      for (uint32_t i = 0; i < FANS_PER_CELL; i++) {
+        if (validation[i]) {
+          if (curr_state & (1 << i)) {
+            // fan still spinning, check if validation expired
+            if (stopwatch_elapsed_ms(validation[i]) >= TOUT_FAN_SET) {
+              // validation expired, reset
+              validation[i] = 0;
+            } else {
+              // still validating, don't recognize spindown as human input
+              curr_state &= ~(1 << i);
             }
+          } else {
+            // fan not spinning, reset validation
+            validation[i] = 0;
+          }
         }
-*/
-#endif
+      }
+
+      // Handle human input
+      if (curr_state != old_state) {
+        fan_set_state(curr_state, 0); // don't validate since human input has no spindown
+      }
+
+      // Check for commands
+      rx_buff_size = UART_GetRxBufferSize();
+      if (rx_buff_size == UART_RX_BUFFER_SIZE) {
+        UART_DEBUG_PutString("\ndata received\n");
+        uint8 rx_cmd = UART_ReadRxData(); // first byte is READ/WRITE
+        if (rx_cmd == UART_READ) {
+          rs485_tx(MASTER_ADDRESS, UART_READ, curr_state); // send back current state
+        } else if (rx_cmd == UART_WRITE) {
+          // next 4 bytes are new state
+          ctrl_state = ((uint32_t) UART_ReadRxData() << 24) |
+            ((uint32_t) UART_ReadRxData() << 16) |
+            ((uint32_t) UART_ReadRxData() << 8) |
+            ((uint32_t) UART_ReadRxData() << 0);
+          rs485_tx(MASTER_ADDRESS, UART_WRITE, curr_state); // send back confirmation that cmd was received
+          curr_state = fan_set_ctrl(curr_state, ctrl_state, validation);
+        } else if (rx_cmd == UART_CONFIG) {
+          uint8_t config_option = UART_ReadRxData(); // get type of config
+          if (config_option == CONFIG_PULSE_TIME) {
+            cell_pulse_time_ms = UART_ReadRxData();
+          } else if (config_option == CONFIG_PWM_FANS) {
+            cell_pwm_time_ms = UART_ReadRxData();
+          }
+        }
+        // Clear RX buffer after processing data
+        UART_ClearRxBuffer();
+        timer_comm = 0;
+      } else if (rx_buff_size != 0) {
+        // If buffer isn't full or empty, clear the buffer if a timeout occurs
+        if (timer_comm == 0) {
+          // Set timer if it hasn't been started yet
+          timer_comm = stopwatch_start();
+        } else {
+          if (stopwatch_elapsed_ms(timer_comm) >= TOUT_RX_COMM) {
+            // Comm timeout: reset timer and clear RX buffer
+            timer_comm = 0;
+            UART_ClearRxBuffer();
+          }
+        }
+      }
     }
+  }
 }
